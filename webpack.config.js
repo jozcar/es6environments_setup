8941156ba4e4477c11e2cff6fbe89be9ab2d22cd
Hector Pacheco <jozcar@yahoo.com>
2017-01-02 13:50:47 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 var webpack = require('webpack');
 var path = require('path');
+var CommonsChunkPlugin = require('./node_modules/webpack/lib/optimize/CommonsChunkPlugin');  // this plugin is usefull to optimize common code, and 
+//to create a vendor bundle file, the vendor are files are your third party libraries, eg. jquery, react,react-dom, etc..
 
 module.exports = { 
     entry: {
         about: './dist/about',
-        contact: './dist/contact'
+        contact: './dist/contact',
+        vendor: ['react','react-dom']
     }, 
     output: {
-        path: path.join(__dirname,'build'),
-        filename: '[name].bundle.js'
-    },
+        path: path.join(__dirname,'build'), // both statements, creates a generic output, by creating a unique bundle, the [name].bundle
+        filename: '[name].bundle.js'        // is replace by the values of the entry points.  so for this configuration, the following files 
+    },                                      //the following files will be created. about.bundle.js, contact.bundle.js,vendor.budle.js file.
      module:{ 
          loaders: [ 
              {
@@ -29,5 +32,8 @@ module.exports = {
                  }
               
         ] 
-    } 
+    } ,
+    plugins: [
+        new CommonsChunkPlugin('vendor','vendor.bundle.js')  //this is nessary to include when bundling vendor code.
+    ]
 }
